0e1d4b2304853fa33f19dc35b8aa08e0be3d38cc
DJW <dan@fake.com>
2019-09-30 18:55:37 +0100

    Added undoMove

--- makemove.s
+++ makemove.s
@@ -50,3 +50,36 @@ store8 r0 r1
 ; Move capPiece into r0 to return it
 mov r0 r4
 ret
+
+label undoMove ; (r0=move, r1=capPiece)
+; put move and capPiece on the stack for now
+push8 r1
+push16 r0
+; flip stm
+mov r0 posStm
+load8 r1 r0
+mov r2 ColourBoth
+xor r1 r1 r2
+store8 r0 r1
+; restore move
+pop16 r0
+; grab to sq ptr
+mov r1 127
+and r1 r0 r1
+mov r2 posArray
+add r1 r1 r2
+; grab piece on to sq (movingPiece, to sq ptr in r1)
+load8 r2 r1
+; restore capPiece on to sq
+pop8 r3 ; grab capPiece
+store8 r1 r3
+; grab from sq ptr
+mov r1 7
+shr r1 r0 r1
+mov r3 127
+and r1 r1 r3
+mov r3 posArray
+add r1 r1 r3
+; place movingPiece back on from sq (movingPiece in r2, from sq ptr in r1)
+store8 r1 r2
+ret
